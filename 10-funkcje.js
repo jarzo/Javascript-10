9fba3e799e09493de1030691a116c252d3ba0580
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 15:09:45 +0200

    finish

--- 10-funkcje.js
+++ 10-funkcje.js
@@ -1,2 +1,35 @@
 'use strict'
 
+/*
+function multiply(parametr1, parametr2, parametr3) {
+	
+	var result;
+	
+	result = parametr1 * parametr2 *parametr3;
+	
+	return result;
+}
+
+var wynik = multiply(3, 9, 10);
+
+console.log("Wynik: " + wynik);
+
+wynik = multiply(35, 2, 10);
+
+
+console.log("Wynik: " + wynik);
+*/
+
+var funkcja = function() {
+	return "wynik :)";
+};
+
+
+
+/*
+
+Funkcja anonimowa
+
+*/
+console.log(funkcja );//traktuje funkcje jako stringa
+console.log( funkcja() );//wykonuje funkcje
